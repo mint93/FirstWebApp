98747f7a5796202d443441ea0495ff4c5c71650d
mint93 <mint5999@gmail.com>
2017-11-05 14:42:46 +0100

    Add-Delete Todo functions

--- my-first-web-app/src/main/java/com/webapp/login/LoginServlet.java
+++ my-first-web-app/src/main/java/com/webapp/login/LoginServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-//
+
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 	private LoginService loginService = new LoginService();
