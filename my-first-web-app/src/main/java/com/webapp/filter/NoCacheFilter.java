78090e3d8f4368c3ae8a6e59ed146a969c6ca986
mint93 <mint5999@gmail.com>
2017-11-21 22:03:47 +0100

    NoCacheFilter added

---
+++ my-first-web-app/src/main/java/com/webapp/filter/NoCacheFilter.java
@@ -0,0 +1,37 @@
+package com.webapp.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+@WebFilter(urlPatterns = { "/add-todo.do", "/delete-todo.do", "/list-todo.do" })
+public class NoCacheFilter implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain chain)
+			throws IOException, ServletException {
+			HttpServletResponse response = (HttpServletResponse) servletResponse;
+			response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+	        response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+	        response.setDateHeader("Expires", 0); // Proxies.
+
+	        chain.doFilter(servletRequest, servletResponse);
+		
+	}
+
+}
